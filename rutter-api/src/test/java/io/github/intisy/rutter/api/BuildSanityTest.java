a9f4cffe5a22abcf87f65597ac6a04782b0b502e
intisy (Finn Birich) <finn@birich.de>
2026-09-05 23:30:14 +0200

    build(gradle): add the gradle skeleton with a java 8 release target

---
+++ rutter-api/src/test/java/io/github/intisy/rutter/api/BuildSanityTest.java
@@ -0,0 +1,13 @@
+package io.github.intisy.rutter.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BuildSanityTest {
+
+    @Test
+    void junit5Runs() {
+        assertEquals(4, 2 + 2);
+    }
+}
